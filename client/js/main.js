b207b84f4edea89952f658bfc2e30a60f9e30e4f
LePhasme <sebastien.courvoisier@gmail.com>
2018-10-22 16:07:51 +0200

    Update project.

--- client/js/main.js
+++ client/js/main.js
@@ -3,13 +3,21 @@ var server;
 $(document).ready(init);
 
 function init() {
-    server = io.connect('http://localhost:8080');
+    server = io.connect('http://192.168.91.68:8080');
+
+    $('#change').on('click', function() {
+        console.log($('#keyword').val())
+        server.emit('keyword', {
+            keyword: $('#keyword').val()
+        });
+    })
 
     server.on('connected', function(data) {
-        $('#keyword').text(data.keyword)
+        $('#keyword').val(data.keyword)
     })
 
     server.on('tweet', function(data) {
+        console.log(data)
         $('<p>' + data.tweet.text + '</p>').appendTo('#tweets')
     })
 }
